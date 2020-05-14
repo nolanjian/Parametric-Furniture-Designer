c48ee8cbff7adeb005c09432481681ea33762b70
nolan <nolanjian@163.com>
2020-05-14 14:26:42 +0800

    none

--- OSGCoreTest/ParametricTreeUnitTest1.cpp
+++ OSGCoreTest/ParametricTreeUnitTest1.cpp
@@ -1,27 +1,27 @@
 #include "ParametricTreeUnitTest1.h"
 #include <gtest\gtest.h>
 #include <OSGIncluding.h>
-#include <ParametricComponent.h>
+#include <Parametric.h>
 
-using namespace PFDCore;
+using namespace PFDComponent;
 
 TEST(ParametricTree, BuildTree) {
 	// Top Level
-	osg::ref_ptr<ParametricComponent> pTop = new ParametricComponent();
+	osg::ref_ptr<Parametric> pTop = new Parametric();
 	EXPECT_NE(pTop.get(), nullptr);
 	pTop->SetParam(L"W = 1200\r\n");
 	pTop->SetParam(L"D = 500\r\n");
 	pTop->SetParam(L"H = 400\r\n");
 
 	// Second Level
-	osg::ref_ptr<ParametricComponent> pSecond1 = new ParametricComponent();
+	osg::ref_ptr<Parametric> pSecond1 = new Parametric();
 	EXPECT_NE(pSecond1.get(), nullptr);
 	pSecond1->SetParam(L"W=Parent.W/3*2");
 	pSecond1->SetParam(L"D=Parent.D");
 	pSecond1->SetParam(L"H=Parent.H");
 
 	// Second Level
-	osg::ref_ptr<ParametricComponent> pSecond2 = new ParametricComponent();
+	osg::ref_ptr<Parametric> pSecond2 = new Parametric();
 	EXPECT_NE(pSecond2.get(), nullptr);
 	pSecond2->SetParam(L"W=Parent.W/3*1");
 	pSecond2->SetParam(L"D=Parent.D");
@@ -52,26 +52,26 @@ TEST(ParametricTree, BuildTree) {
 }
 
 TEST(ParametricTree, BuildTree1) {
-	osg::ref_ptr<ParametricComponent> pNode0 = new ParametricComponent();
+	osg::ref_ptr<Parametric> pNode0 = new Parametric();
 	EXPECT_NE(pNode0.get(), nullptr);
 	pNode0->SetParam(L"W = 32");
 
-	osg::ref_ptr<ParametricComponent> pNode1 = new ParametricComponent();
+	osg::ref_ptr<Parametric> pNode1 = new Parametric();
 	EXPECT_NE(pNode1.get(), nullptr);
 	pNode1->SetParam(L"W = Parent.W/2");
 	pNode0->addChild(pNode1);
 
-	osg::ref_ptr<ParametricComponent> pNode2 = new ParametricComponent();
+	osg::ref_ptr<Parametric> pNode2 = new Parametric();
 	EXPECT_NE(pNode2.get(), nullptr);
 	pNode2->SetParam(L"W = Parent.W/2");
 	pNode1->addChild(pNode2);
 
-	osg::ref_ptr<ParametricComponent> pNode3 = new ParametricComponent();
+	osg::ref_ptr<Parametric> pNode3 = new Parametric();
 	EXPECT_NE(pNode3.get(), nullptr);
 	pNode3->SetParam(L"W = Parent.W/2");
 	pNode2->addChild(pNode3);
 
-	osg::ref_ptr<ParametricComponent> pNode4 = new ParametricComponent();
+	osg::ref_ptr<Parametric> pNode4 = new Parametric();
 	EXPECT_NE(pNode4.get(), nullptr);
 	pNode4->SetParam(L"W = Parent.W/2");
 	pNode3->addChild(pNode4);
@@ -86,7 +86,7 @@ TEST(ParametricTree, BuildTree1) {
 }
 
 TEST(ParametricTree, CalTest) {
-	osg::ref_ptr<ParametricComponent> pc = new ParametricComponent();
+	osg::ref_ptr<Parametric> pc = new Parametric();
 	EXPECT_NE(pc.get(), nullptr);
 
 	pc->SetParam(L" A = 800 ");
@@ -111,7 +111,7 @@ TEST(ParametricTree, CalTest) {
 TEST(ParametricTree, RegexTest1)
 {
 	std::wstring strKey, strVal;
-	ParametricComponent::regexParseFormular(L" A = 800 ", strKey, strVal);
+	Parametric::regexParseFormular(L" A = 800 ", strKey, strVal);
 	EXPECT_STREQ(strKey.c_str(), L"A");
 	EXPECT_STREQ(strVal.c_str(), L"800");
 }
@@ -119,7 +119,7 @@ TEST(ParametricTree, RegexTest1)
 TEST(ParametricTree, RegexTest2)
 {
 	std::wstring strKey, strVal;
-	ParametricComponent::regexParseFormular(L"     \r\n       B       \r\n      =               \r\n           800        \r\n         ",
+	Parametric::regexParseFormular(L"     \r\n       B       \r\n      =               \r\n           800        \r\n         ",
 		strKey, strVal);
 	EXPECT_STREQ(strKey.c_str(), L"B");
 	EXPECT_STREQ(strVal.c_str(), L"800");
@@ -128,7 +128,7 @@ TEST(ParametricTree, RegexTest2)
 TEST(ParametricTree, RegexTest3)
 {
 	std::wstring strKey, strVal;
-	ParametricComponent::regexParseFormular(L" W=800 ", strKey, strVal);
+	Parametric::regexParseFormular(L" W=800 ", strKey, strVal);
 	EXPECT_STREQ(strKey.c_str(), L"W");
 	EXPECT_STREQ(strVal.c_str(), L"800");
 }
@@ -136,7 +136,7 @@ TEST(ParametricTree, RegexTest3)
 TEST(ParametricTree, RegexTest4)
 {
 	std::wstring strKey, strVal;
-	ParametricComponent::regexParseFormular(L"Q=800", strKey, strVal);
+	Parametric::regexParseFormular(L"Q=800", strKey, strVal);
 	EXPECT_STREQ(strKey.c_str(), L"Q");
 	EXPECT_STREQ(strVal.c_str(), L"800");
 }
@@ -145,7 +145,7 @@ TEST(ParametricTree, RegexTest5)
 {
 	std::wstring str(L" 18 ");
 
-	ParametricComponent::regexParseKV(str);
+	Parametric::regexParseKV(str);
 	EXPECT_STREQ(str.c_str(), L"18");
 }
 
@@ -153,7 +153,7 @@ TEST(ParametricTree, RegexTest6)
 {
 	std::wstring str(L"   \r\n   \r\n   W1\r\n");
 
-	ParametricComponent::regexParseKV(str);
+	Parametric::regexParseKV(str);
 	EXPECT_STREQ(str.c_str(), L"W1");
 }
 
@@ -161,7 +161,7 @@ TEST(ParametricTree, RegexTest7)
 {
 	std::wstring str(L"   \r\n   \r\n   W1\r\n=");
 
-	ParametricComponent::regexParseKV(str);
+	Parametric::regexParseKV(str);
 	EXPECT_STREQ(str.c_str(), L"W1");
 }
 
@@ -169,27 +169,27 @@ TEST(ParametricTree, RegexTest8)
 {
 	std::wstring str(L"   \r\n   \r\n   W1=  \r\n");
 
-	ParametricComponent::regexParseKV(str);
+	Parametric::regexParseKV(str);
 	EXPECT_STREQ(str.c_str(), L"W1");
 }
 
 TEST(ParametricTree, RegexTest9)
 {
 	std::wstring str(L"   \r\n   \r\n     \r\n");
-	bool bRet = ParametricComponent::regexParseKV(str);
+	bool bRet = Parametric::regexParseKV(str);
 	EXPECT_EQ(bRet, false);
 
 	std::wstring str1(L"");
-	bool bRet1 = ParametricComponent::regexParseKV(str1);
+	bool bRet1 = Parametric::regexParseKV(str1);
 	EXPECT_EQ(bRet1, false);
 }
 
 TEST(ParametricTree, RegexTest10)
 {
 	std::wstring strKey, strVal;
-	bool bRet = ParametricComponent::regexParseFormular(L"Q", strKey, strVal);
+	bool bRet = Parametric::regexParseFormular(L"Q", strKey, strVal);
 	EXPECT_EQ(bRet, false);
 
-	bool bRet1 = ParametricComponent::regexParseFormular(L"Q=", strKey, strVal);
+	bool bRet1 = Parametric::regexParseFormular(L"Q=", strKey, strVal);
 	EXPECT_EQ(bRet1, false);
 }
